a130605f2a05766040c1449617d38a5291f2c1ba
George Yegon <georgeyegon67@gmail.com>
2025-01-26 23:48:33 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -16,9 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -30,6 +28,7 @@ class Plants(Resource):
             name=data['name'],
             image=data['image'],
             price=data['price'],
+            is_in_stock=data.get('is_in_stock', True)  # Default value for is_in_stock
         )
 
         db.session.add(new_plant)
@@ -37,16 +36,44 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"message": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        data = request.get_json()
+
+        # Update 'is_in_stock' if provided in the request data
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        # Update 'price' if provided in the request data
+        if 'price' in data:
+            plant.price = data['price']
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
